12ce0333e960dc2cc5e148f3e3b33d37ab4a0bbf
Danny Wolf <2dawolf@gmail.com>
2015-11-30 07:14:06 -0500

    Kinda sorta works.

--- gravity.cu
+++ gravity.cu
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-#define N 2048 
-#define THREADS_PER_BLOCK N 
+#define N 512 
+#define THREADS_PER_BLOCK 512 
 
-#define GRAVITATIONAL_CONSTANT 66.7 // km^3 / (Yg * s^2)
+//#define GRAVITATIONAL_CONSTANT 66.7 // km^3 / (Yg * s^2)
+#define GRAVITATIONAL_CONSTANT 240300.0 // km^3 / (Yg * min^2)
 // http://www.wolframalpha.com/input/?i=gravitational+constant+in+km%5E3%2F%28Yg+*+s%5E2%29
 
 __constant__ double G;
@@ -16,12 +17,12 @@ void random_ints(int* a, int num) {
         }
 } 
 
-void random_doubles(double* a, int num) {
+void random_doubles(double* a, int num, double multiplier) {
         int i;
-        for(i = 0; i < num; ++i) {
-                a[i] = (double)rand() / (double)RAND_MAX;
+        for(i = 0; i < num; i++) {
+                a[i] = (double)rand() / (double)RAND_MAX * multiplier;
         }
-} 
+}
 
 __global__ void add(int *a, int *b, int *c) {
         int index = threadIdx.x + blockIdx.x * blockDim.x;
@@ -47,16 +48,31 @@ __global__ void dot(int *a, int *b, int *c) {
         }
 }
 
-__global__ void gravity(double *x, double *y, double *m, double *accel) {
+__global__ void update_positions(double *x, double *y, double *vx, double *vy) {
+        x[blockIdx.x] += vx[blockIdx.x] * 1.0;
+        y[blockIdx.x] += vy[blockIdx.x] * 1.0;
+}
+
+__global__ void gravity(double *x, double *y, double *m, double *vx, double *vy) {
         __shared__ double accel_parts[THREADS_PER_BLOCK];
-        int index = threadIdx.x + blockIdx.x * blockDim.x;
+        //int index = threadIdx.x + blockIdx.x * blockDim.x;
 
-        double d_x = x[blockIdx.x] - x[threadIdx.x];
-        double d_y = y[blockIdx.x] - y[threadIdx.x];
+        double d_x, d_y, dist_sq;
 
-        double dist_sq = d_x * d_x + d_y * d_y; 
+        if(blockIdx.x != threadIdx.x) {
+                d_x = x[blockIdx.x] - x[threadIdx.x];
+                d_y = y[blockIdx.x] - y[threadIdx.x];
 
-        accel_parts[threadIdx.x] = 66.7 * m[threadIdx.x] / dist_sq; 
+                dist_sq = d_x * d_x + d_y * d_y; 
+                
+                if(dist_sq > 10) {
+                        accel_parts[threadIdx.x] = 66.7 * m[threadIdx.x] / dist_sq;
+                } else {
+                        accel_parts[threadIdx.x] = 0.0;
+                }
+        } else {
+                accel_parts[threadIdx.x] = 0.0;
+        }
 
         /*
            G * M * m
@@ -72,14 +88,14 @@ __global__ void gravity(double *x, double *y, double *m, double *accel) {
                 for(int i = 0; i < blockDim.x; i++) {
                         sum += accel_parts[i];
                 }
-                accel[blockIdx.x] = sum;
+                vx[blockIdx.x] = sum * (d_x / sqrt(dist_sq));
+                vy[blockIdx.x] = sum * (d_y / sqrt(dist_sq));
         }
 }
 
-
 int main(void) {
-        double *x, *y, *m, *acc;
-        double *dev_x, *dev_y, *dev_m, *dev_acc;
+        double *x, *y, *m, *vx, *vy;
+        double *dev_x, *dev_y, *dev_m, *dev_vx, *dev_vy;
         int size = N * sizeof(double);
 
         /*cudaMemcpyToSymbol(G,
@@ -92,38 +108,60 @@ int main(void) {
         cudaMalloc((void**)&dev_x, size);
         cudaMalloc((void**)&dev_y, size);
         cudaMalloc((void**)&dev_m, size);
-        cudaMalloc((void**)&dev_acc, size);
+        cudaMalloc((void**)&dev_vx, size);
+        cudaMalloc((void**)&dev_vy, size);
 
         x = (double*)malloc(size);
         y = (double*)malloc(size);
         m = (double*)malloc(size);
-        acc = (double*)malloc(size);
+        vx = (double*)malloc(size);
+        vy = (double*)malloc(size);
 
-        random_doubles(x, N);
-        random_doubles(y, N);
-        random_doubles(m, N);
+        int seed = time(NULL);
+        srand(seed);
+
+        random_doubles(x, N, 5e4);
+        random_doubles(y, N, 5e4);
+        random_doubles(m, N, 0.5);
 
         cudaMemcpy(dev_x, x, size, cudaMemcpyHostToDevice);
         cudaMemcpy(dev_y, y, size, cudaMemcpyHostToDevice);
         cudaMemcpy(dev_m, m, size, cudaMemcpyHostToDevice);
 
-        gravity<<<N, THREADS_PER_BLOCK>>>(dev_x, dev_y, dev_m, dev_acc);
+        FILE *fp = fopen("locations.csv", "w");
 
-        cudaMemcpy(acc, dev_acc, size, cudaMemcpyDeviceToHost);
+
+        for(int i = 0; i < 100; i++) {
+                update_positions<<<N, 1>>>(dev_x, dev_y, dev_vx, dev_vy);
+                gravity<<<N, THREADS_PER_BLOCK>>>(dev_x, dev_y, dev_m, dev_vx, dev_vy);
+
+                cudaMemcpy(x, dev_x, size, cudaMemcpyDeviceToHost);
+                cudaMemcpy(y, dev_y, size, cudaMemcpyDeviceToHost);
+                cudaMemcpy(vx, dev_vx, size, cudaMemcpyDeviceToHost);
+                cudaMemcpy(vy, dev_vy, size, cudaMemcpyDeviceToHost);
+
+                //printf("%g\n", (double)i / (double)ITERATIONS);
+
+                for(int j = 0; j < N; j++)
+                fprintf(fp, "%d, %d, %g, %g, %g, %g\n", i, j, x[j], y[j], vx[j], vy[j]);
+        }
+
+        fclose(fp);
 
         cudaFree(dev_x);
         cudaFree(dev_y);
         cudaFree(dev_m);
-        cudaFree(dev_acc);
+        cudaFree(dev_vx);
+        cudaFree(dev_vy);
         
-        free(x); free(y); free(m); 
+        free(x); free(y); free(m);
 
-        printf("Numbers:\n");
-        for(int i = 0; i < N; i++) {
-                printf("%d\n", acc[i]);
-        }
-
-        free(acc);
+        //printf("Numbers:\n");
+        //for(int i = 0; i < N; i++) {
+        //        printf("%d: (%g, %g)\n", i, vx[i], vy[i]);
+        //}
+        
+        free(vx); free(vy);
 
         return 0;
 }
---
+++ render.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+import pandas as pd
+import pygame
+from pygame import gfxdraw
+import sys
+
+
+size = [800, 800]
+screen = pygame.display.set_mode(size)
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+color = pygame.Color.g
+
+clock = pygame.time.Clock()
+
+locations = pd.read_csv('locations2.csv')
+#import pdb; pdb.set_trace()
+
+x = locations.ix[:,2]
+y = locations.ix[:,3]
+xv = locations.ix[:,4]
+yv = locations.ix[:,5]
+
+scale = 100.0
+
+while True:
+    clock.tick(10)
+    for t in range(100):
+        print 'tick'
+        screen.fill(BLACK)
+        for l in range(512):
+            i = t + l
+            #print (x[i]/scale,y[i]/scale)
+            pygame.draw.line(screen, GREEN, [x[i]/scale, y[i]/scale], [x[i]/scale + xv[i], y[i]/scale + yv[i]], 1)
+	#gfxdraw.pixel(surface, l.x, l.y, color)
+   
+        pygame.display.update() 
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             pygame.quit(); sys.exit();
