112fe6f8e4918f6287503240cf4f77d38c073d29
Danny Wolf <wolf@students.olin.edu>
2015-12-09 23:45:46 -0800

    Param sweep fix.

--- param_sweep.sh
+++ param_sweep.sh
@@ -5,14 +5,14 @@ mkdir -p sweep;
 mkdir -p sweep/inputs;
 mkdir -p sweep/saved;
 mkdir -p sweep/outputs;
-for v in $(./linspace.sh 0.5 4.0 100);
+for m in $(./linspace.sh 0.5 4.0 100);
 do
         echo $m;
-        ./explode.py -m $m -n 994;
-        cp input.csv sweep/inputs/$m-input.csv;
+        ./explode.py -m $m;
+        cp input.csv "sweep/inputs/input-$m.csv";
         ./bin/grav;
-        mv output.csv sweep/outputs/$m-output.csv;
-        mv next-input.csv sweep/saved/$m-next-input.csv;
+        mv output.csv "sweep/outputs/output-$m.csv";
+        mv next-input.csv "sweep/saved/next-input-$m.csv";
 done
 
 echo "Done!";
